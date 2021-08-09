7279b4cb1e2c1c7c060e121f9cbd7e211c656278
phillip.hqs <63358485+philliphqs@users.noreply.github.com>
2021-08-10 00:08:09 +0200

    Install script for Linux
    And some file structure changes

---
+++ StreamDeck-Client/command.sh
@@ -0,0 +1,7 @@
+function StreamDeck() {
+  echo '------------------------------'
+  echo '| Starting StreamDeck-Client |'
+  echo '------------------------------'
+  cd ~/etc/StreamDeck-0.0.2-alpha/StreamDeck-Client
+  python -m StreamDeck-Client.py
+}
