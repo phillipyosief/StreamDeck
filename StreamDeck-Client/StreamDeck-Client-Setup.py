5d5cdf84929ebd23108ea608b2d143e9731c79d1
phillip.hqs <63358485+philliphqs@users.noreply.github.com>
2021-08-14 02:40:32 +0200

    Update StreamDeck-Client-Setup.py

--- StreamDeck-Client/StreamDeck-Client-Setup.py
+++ StreamDeck-Client/StreamDeck-Client-Setup.py
@@ -128,8 +128,8 @@ def download_sdclient():
     print('\n')
     os.system('cd /home/pi/')
     os.system('rm -r StreamDeck')
-    subprocess.call('cd /home/pi')
-    subprocess.call(['git', 'clone', 'https://github.com/philliphqs/StreamDeck'], stdout=subprocess.DEVNULL)
+
+    subprocess.call(['git', 'clone', 'https://github.com/philliphqs/StreamDeck', '/home/pi/'], stdout=subprocess.DEVNULL)
 
 
 def check_platform():
